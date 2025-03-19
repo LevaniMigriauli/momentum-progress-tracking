f377aa6177516dcebe272c8ec137a4ac1114df82
Levani migriauli <levani.migriauli@lb.ge>
2025-03-20 01:50:21 +0400

    refactor: prettify

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
-import Layout from "./components/layout/Layout.jsx";
-import TasksHome from "./pages/tasksHome/TasksHome.jsx";
-import TaskDetails from "./pages/TaskDetails.jsx";
-import CreateTask from "./pages/createTask/CreateTask.jsx";
-import {AppProvider} from "./context/appContext.jsx";
+import Layout from './components/layout/Layout.jsx'
+import TasksHome from './pages/tasksHome/TasksHome.jsx'
+import TaskDetails from './pages/taskDetails/TaskDetails.jsx'
+import CreateTask from './pages/createTask/CreateTask.jsx'
+import { AppProvider } from './context/appContext.jsx'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function App() {
-
   return (
-      <QueryClientProvider client={queryClient}>
-        <AppProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout/>}>
-                <Route index element={<TasksHome/>}/>
-                <Route path="/task-details/:id" element={<TaskDetails/>}/>
-                <Route path="/create-task" element={<CreateTask/>}/>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </AppProvider>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<TasksHome />} />
+              <Route path="/task-details/:id" element={<TaskDetails />} />
+              <Route path="/create-task" element={<CreateTask />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
+    </QueryClientProvider>
   )
 }
 
