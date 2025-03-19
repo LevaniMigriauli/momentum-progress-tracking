19442cc57ea0d8f50a241b568ebe76b13def17ab
Levani migriauli <levani.migriauli@lb.ge>
2025-03-20 02:20:19 +0400

    refactor: set dynamic Content-Type, prettify

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,18 +1,19 @@
-import axios from "axios"
+import axios from 'axios'
 
-const token = "9e6c5dce-6c15-4bde-bd58-e850ce4cde4d"
-const BASE_URL = "https://momentum.redberryinternship.ge/api"
+const token = '9e6c5dce-6c15-4bde-bd58-e850ce4cde4d'
+const BASE_URL = 'https://momentum.redberryinternship.ge/api'
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
 })
 
 apiClient.interceptors.request.use(
   (config) => {
-    config.headers["Authorization"] = `Bearer ${token}`
+    config.headers['Authorization'] = `Bearer ${token}`
+
+    if (!config.headers['Content-Type']) {
+      config.headers['Content-Type'] = `application/json`
+    }
     return config
   },
   (error) => {
@@ -24,34 +25,34 @@ apiClient.interceptors.response.use(
     return response
   },
   (error) => {
-    let errorMessage = "An error occurred"
+    let errorMessage = 'An error occurred'
 
     if (error.response) {
       const status = error.response.status
       switch (status) {
         case 400:
-          errorMessage = "Bad Request"
+          errorMessage = 'Bad Request'
           break
         case 401:
-          errorMessage = "Unauthorized. Please login."
+          errorMessage = 'Unauthorized. Please login.'
           break
         case 404:
-          errorMessage = "Resource not found"
+          errorMessage = 'Resource not found'
           break
         case 500:
-          errorMessage = "Server error. Please try again later."
+          errorMessage = 'Server error. Please try again later.'
           break
         default:
-          errorMessage = "Unexpected error occurred"
+          errorMessage = 'Unexpected error occurred'
           break
       }
     } else if (error.request) {
-      errorMessage = "No response from server. Please check your connection."
+      errorMessage = 'No response from server. Please check your connection.'
     } else {
       errorMessage = `Error: ${error.message}`
     }
 
-    console.error("API Error:", errorMessage)
+    console.error('API Error:', errorMessage)
 
     return Promise.reject(new Error(errorMessage))
   },
--- src/api/employees.js
+++ src/api/employees.js
@@ -1,9 +1,15 @@
-import apiClient from "./apiClient.js";
+import apiClient from './apiClient.js'
 
 export const getEmployees = async () => {
-  return apiClient.get("/employees").then((res) => res.data);
-};
+  return apiClient.get('/employees').then((res) => res.data)
+}
 
 export const createEmployees = async (form) => {
-  return apiClient.post("/employees", form).then((res) => res.data);
-};
+  return apiClient
+    .post('/employees', form, {
+      headers: {
+        'Content-type': 'multipart/form-data',
+      },
+    })
+    .then((res) => res.data)
+}
